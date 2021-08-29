1927dea6e96168b375a604499db6d81eeb38eb58
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-29 19:21:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AccessModifierDemo;
+
+namespace AccessSpacifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 cl1 = new Class1();
+            cl1.publicmethod();
+            //cl1.intrnalmethod(); // can not access or call private method out side its namespace
+            //cl1.protintmeth();   // can not access or call private method out side its namespace
+            //cl1.privatemethod(); // can not access or call private method out side its namespace
+            //cl1.protectedmethod(); // can not access or call private method out side its namespace
+            Demo D = new Demo();
+            D.print();
+        }
+    }
+
+}
